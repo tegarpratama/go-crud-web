4ab9219d9b3246ed18ad3a395bb7712f305d4fb2
Tegar Pratama <tegar.pratama@kanggo.id>
2023-01-08 13:37:08 +0700

    update main

--- main.go
+++ main.go
@@ -17,18 +17,18 @@ func main() {
 	// 1.Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
 
-	// 2. Products
-	http.HandleFunc("/products", productcontroller.Index)
-	http.HandleFunc("/products/add", productcontroller.Add)
-	http.HandleFunc("/products/detail", productcontroller.Detail)
-	http.HandleFunc("/products/edit", productcontroller.Edit)
-	http.HandleFunc("/products/delete", productcontroller.Delete)
-
 	// 2. Category
 	http.HandleFunc("/categories", categorycontroller.Index)
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// 3. Products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	// Run server
 	log.Println("Server running on port: 8080")
